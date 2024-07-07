51660a70de53756646f820a9268b946f15badd3c
laouali80 <120820961+laouali80@users.noreply.github.com>
2024-07-07 23:13:37 +0100

    Update urls.py

--- task/urls.py
+++ task/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('auth/register', views.register, name='register'),
     path('auth/login', views.login, name='login'),
     path('api/users/<str:id>', views.get_user_record, name='get_user'),
-    path('api/organisations/', views.get_or_create_organisations, name='get_or_create_organisations'),
+    path('api/organisations', views.get_or_create_organisations, name='get_or_create_organisations'),
     path('api/organisations/<str:orgId>', views.get_organisation, name='get_organisation'),
     
     path('api/organisations/<str:orgId>/users', views.add_user, name='add_user'),
